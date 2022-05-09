38ae0ff300d69d05f25c27fdffb73181f8efefdb
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-05-09 16:06:36 -0600

    ReductJs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
-import { CartIcon } from '../icons'
+import { CartIcon, Testing } from '../icons'
 import { useSelector } from 'react-redux'
 
-const Navbar = () => {
-    console.log(useSelector(() => { }));
+export const Navbar = () => {
+    const amount = useSelector((store) => store.cart.amount)
     return (
         <nav>
+            {/* <Testing /> */}
             <div class='nav-center'>
                 <h3>redux toolkit</h3>
                 <div class='nav-container'>
                     <CartIcon />
                     <div className="amount-container">
-                        <p className="total-amount">0</p>
+                        <p className="total-amount">{amount}</p>
                     </div>
                 </div>
             </div>
